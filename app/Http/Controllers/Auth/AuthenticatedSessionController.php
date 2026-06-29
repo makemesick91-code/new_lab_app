d0bea931378e836f6ceca468856cb2e452978c16
Raushan Fikri Ridha <makemesick91@gmail.com>
2026-06-29 18:31:31 +0800

    fix: import Auth facade for logout after online context

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Facades\Auth;
+
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Modules\RmeOnlineContext\Services\UserOnlineContextService;
